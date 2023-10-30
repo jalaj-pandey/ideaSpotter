3f32546c40fac5e08b8b5d5f878905e2d43e797d
Jalaj Pandey <jalajpandey123@gmail.com>
2023-10-31 00:39:56 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose =  require('mongoose');
+
+
+
+const connectToMongo=()=>{
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to mongo");
+}
+
+module.exports = connectToMongo;
+
